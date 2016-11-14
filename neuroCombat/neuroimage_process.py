0e95f4d0ac17f768735d5cb4c3181137ebfb775f
Nicholas Cullen <nicholas.c.cullen.th@dartmouth.edu>
2016-11-14 16:11:30 -0500

    neuroimaging process

--- neuroCombat/neuroimage_process.py
+++ neuroCombat/neuroimage_process.py
@@ -1,34 +1,6 @@
 """
 Functions for processing neuroimaging files in order
 to be fed into neuroCombat function
-
-
-from nilearn.input_data import NiftiMasker
-import os
-
-mask_img = '/users/nick/desktop/pbac_images/mask/mask.nii.gz'
-
-masker = NiftiMasker()
-masker.fit(mask_img)
-
-img_dir = '/users/nick/desktop/pbac_images/images/'
-
-img_paths = os.listdir(img_dir)
-try:
-	img_paths.remove('.DS_Store')
-except:
-	pass
-img_paths = [img_dir+p for p in img_paths]
-
-masked_data = masker.transform(img_paths)
-
-### run combat on masked_data ###
-#combat_data = neuroCombat(masked_data, ...)
-
-### inverse transform to get 3D images back from numpy array
-#corrected_imgs = masker.inverse_transform(combat_data)
-
-## save the images
 """
 
 import os
